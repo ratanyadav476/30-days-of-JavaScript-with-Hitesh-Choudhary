a657e4f2dcdccbed6bfe905588fc8de318261d00
Ratan Yadav <143277039+ratanyadav476@users.noreply.github.com>
2024-07-13 10:40:57 +0530

    understanding variables datatypes deeply

---
+++ Day-1/feature-request.js
@@ -0,0 +1,24 @@
+let num=2345
+console.log(`the ${typeof num} is ${num}`) ;
+
+let bool=true;
+
+console.log(`the ${typeof bool} is ${bool}`) ;
+
+let str="pritee";
+
+console.log(`the ${typeof str} is ${str}`) ;
+
+let arr=["ratan","krishna","prateek","adnan","nitesh"]
+
+console.log(`the ${typeof arr} is ${arr}`) ;
+
+let obj={
+    name:"Miss yadav",
+    education:8,
+    position:"mon with housewife",
+    husband:"Mr  yadav"
+}
+
+console.log(`the ${typeof obj} is ${obj}`) ;
+console.log(obj);
