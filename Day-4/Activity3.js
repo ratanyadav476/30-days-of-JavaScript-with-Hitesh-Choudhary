cacf56b92928cbf80cfa7b1892af848add7927c5
Ratan Yadav <143277039+ratanyadav476@users.noreply.github.com>
2024-07-19 23:31:00 +0530

    Activity-3

---
+++ Day-4/Activity3.js
@@ -0,0 +1,16 @@
+// do -while loop 
+console.log("numbers----------");
+let i=1
+do {
+   console.log(i); 
+   i++
+} while (i<=5);
+console.log("factorial-----------");
+let n=5;
+let fact=1;
+let j=1;
+do {
+    fact *=j
+    console.log(fact);
+    j++
+} while (j<=n);
