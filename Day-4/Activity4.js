b8867f2c931393cd200d599d61cb06b659873d61
Ratan Yadav <143277039+ratanyadav476@users.noreply.github.com>
2024-07-19 23:31:28 +0530

    Activity-4

---
+++ Day-4/Activity4.js
@@ -0,0 +1,12 @@
+console.log("printing pattern");
+// *
+// * *
+// * * * 
+// * * * * 
+// * * * * *
+let n=5
+for (let line = 1; line <= n; line++) {
+  let str="*";
+  console.log(str.repeat(line));
+   
+}
