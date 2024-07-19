74840e370301eeca5725fff754f7e126fd837942
Ratan Yadav <143277039+ratanyadav476@users.noreply.github.com>
2024-07-19 23:30:32 +0530

    Activity-2

---
+++ Day-4/Activity2.js
@@ -0,0 +1,15 @@
+// while loop 
+// nums sum from 1to 10 
+let i=0;
+let sum=0;
+while (i<10) {
+    sum +=i;
+   console.log(sum); 
+   i++
+}
+
+let j=10;
+while (j>=0) {
+    console.log(j);
+    j--
+}
