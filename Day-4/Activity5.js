5534b9fad6b9b7a10a6115a6092e5eacfee41bbe
Ratan Yadav <143277039+ratanyadav476@users.noreply.github.com>
2024-07-19 23:31:58 +0530

    Activity-5

---
+++ Day-4/Activity5.js
@@ -0,0 +1,13 @@
+console.log("continue----------------");
+for(let i=0;i<=10;i++){
+    if (i==5) continue;
+    console.log(i);
+
+}
+
+console.log("break--------------------");
+for(let i=0;i<=10;i++){
+    if (i==7) break;
+    console.log(i);
+
+}
