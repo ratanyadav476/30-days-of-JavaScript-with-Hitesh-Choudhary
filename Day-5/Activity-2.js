eafcb597cdb187fbefbd6725e0d07e2354fc9312
Ratan Yadav <143277039+ratanyadav476@users.noreply.github.com>
2024-07-20 21:43:17 +0530

    Activity2.js

---
+++ Day-5/Activity-2.js
@@ -0,0 +1,14 @@
+let fun=(a,b)=>{
+if (a>b) {
+    console.log(a+" is maximum");
+}else{
+    console.log(b+" is maximum");
+}
+}
+fun(7,9)
+
+
+let concatanate=( a,b)=>{
+    console.log(a+b);
+}
+concatanate("hari ","Ratan")
