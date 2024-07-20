bbb046a19c62b7f552bdcbf78bd0bb0bef141fea
Ratan Yadav <143277039+ratanyadav476@users.noreply.github.com>
2024-07-20 21:42:43 +0530

    Activity1.js

---
+++ Day-5/Activity1.js
@@ -0,0 +1,14 @@
+function isEvenorOdd(n){
+    if (n%2==0) {
+        console.log(n+" is even");
+    }else{
+        
+        console.log(n+" is odd");
+    }
+}
+isEvenorOdd(6)
+isEvenorOdd(5)
+
+// square of number 
+let square=(n)=>n*n
+console.log(square(6));
