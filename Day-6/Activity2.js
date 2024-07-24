d06a47f9619157977a41284bfbe6f81a5fcae490
Ratan Yadav <143277039+ratanyadav476@users.noreply.github.com>
2024-07-24 16:16:49 +0530

    Activity2.js

---
+++ Day-6/Activity2.js
@@ -0,0 +1,15 @@
+
+// push operation 
+let arr=[1,2,3,4,5]
+arr.push(6)
+console.log(arr);
+// pop operation 
+arr.pop()
+console.log(arr);
+
+// shift operation
+arr.shift(0)
+console.log(arr);
+
+arr.unshift(1)
+console.log(arr);
