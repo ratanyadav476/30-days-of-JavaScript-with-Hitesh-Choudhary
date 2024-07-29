2b00f488095ccf869540813480235fd4a7196a9c
Ratan Yadav <143277039+ratanyadav476@users.noreply.github.com>
2024-07-29 16:37:01 +0530

    Activity5.js

---
+++ Day7/Activity5.js
@@ -0,0 +1,22 @@
+let salary = [
+    ["ABC", 24, 18000],
+    ["EFG", 30, 30000],
+    ["IJK", 28, 41000],
+    ["EFG", 31, 28000],
+ ];
+ console.log(salary);
+
+ for (const sal of salary) {
+    console.log(sal);
+ }
+//  console.log(salary[0][2]);
+//  console.log(salary[1][2]);
+//  console.log(salary[2][2]);
+//  console.log(salary[0][1]);
+//  console.log(salary[1][1]);
+//  console.log(salary[2][1]);
+//  console.log(salary[3][1]);
+//  console.log(salary[0][0]);
+//  console.log(salary[1][0]);
+//  console.log(salary[2][0]);
+//  console.log(salary[3][0]);
